1f9d336e8709bd79268b0ab7d4e34eeb701db1d9
mawentao007 <1320963812@qq.com>
2015-09-14 16:37:28 +0800

    comments

--- simple-cf/cf.py
+++ simple-cf/cf.py
@@ -25,18 +25,30 @@ def itemBasedCF(data):
 
 
     for i in range(0,len(data_ibs.columns)):
-        #取ib中cos距离最近的10个元素的索引
-        data_neighbours.ix[i,:10] = data_ibs.ix[0:,i].order(ascending=False)[:10].index
+        #取ib中cos距离最近的10个元素的索引,也就是相似度最高的十首歌曲
+        data_neighbours.ix[i,:10] = data_ibs.ix[:,i].order(ascending=False)[:10].index
 
     return data_neighbours
 
 
 def userBasedCF(data,data_neighbours):
+
+# Have an Item Based similarity matrix at your disposal (we do…wohoo!)
+# Check which items the user has consumed
+# For each item the user has consumed, get the top X neighbours
+# Get the consumption record of the user for each neighbour.
+# Calculate a similarity score using some formula
+# Recommend the items with the highest score
+#这个算法实现和我想的不太一样
+
+
     def getScore(history,similarities):
         return sum(history * similarities)/sum(similarities)
 
-#行索引为
+#行索引为用户,列索引为歌曲名,data.index不等于用户编号
     data_sims = pd.DataFrame(index=data.index,columns=data.columns)
+
+#data_sims的用户列进行初始化
     data_sims.ix[:,:1] = data.ix[:,:1]
 
     data_germany = data.drop('user',1)
@@ -46,19 +58,26 @@ def userBasedCF(data,data_neighbours):
         for j in range(1,len(data_sims.columns)):
             user = data_sims.index[i]
             product = data_sims.columns[j]
-            print product
 
+#相应歌曲被打分过,忽略
             if data.ix[i][j] == 1:
                 data_sims.ix[i][j] = 0
             else:
+                #和当前歌曲相似度最高的十首
                 product_top_names = data_neighbours.ix[product][1:10]
+                #和当前歌曲相似度最高的十首的cos距离
                 product_top_sims = data_ibs[product].order(ascending=False)[1:10]
+                #用户是否购买相应歌曲
                 user_purchases = data_germany.ix[user,product_top_names]
 
+                #通过购买情况和相似度计算用户相应歌曲的分数
                 data_sims.ix[i][j] = getScore(user_purchases,product_top_sims)
 
+#
     data_recommend = pd.DataFrame(index=data_sims.index,columns=['user','1','2','3','4','5','6'])
-    data_recommend.ix[0:,0] = data_sims.ix[:,0]
+#第0列是user列,下面语句就是初始化该列,注意第0列不是index列.
+    data_recommend.ix[:,0] = data_sims.ix[:,0]
+
 
     for i in range(0,len(data_sims.index)):
         data_recommend.ix[i,1:] = data_sims.ix[i,:].order(ascending=False).ix[1:7,].index.transpose()
@@ -66,8 +85,9 @@ def userBasedCF(data,data_neighbours):
     print data_recommend.ix[:10,:4]
 
 if __name__ == "__main__":
-     data = loadData("data.csv")
+     data = loadData("small.csv")
      data_neighbours = itemBasedCF(data)
+
      userBasedCF(data,data_neighbours)
 
     # print data_neighbours.head(6).ix[:6,2:4]
